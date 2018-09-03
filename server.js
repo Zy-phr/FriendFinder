6bdeaa8c3ddbdf41b473709dd85c44de0a704664
Zy-phr <richard.sanchez79@yahoo.com>
2018-09-02 19:23:18 -0500

    heroku

--- server.js
+++ server.js
@@ -18,9 +18,7 @@ var PORT = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, "app/public")));
 	
 // implement bodyParser
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // ===========================================================================
