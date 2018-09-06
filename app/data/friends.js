1c6647ef6bf8485ce76ffeb949ef287d20784ebf
Zy-phr <richard.sanchez79@yahoo.com>
2018-09-06 17:30:56 -0500

    Modified Friend-Finder-app

---
+++ app/data/friends.js
@@ -0,0 +1,51 @@
+var friends =[
+    {
+      "name":"Kaley Cuoco",
+      "photo":"hhttps://image.tmdb.org/t/p/w1280/dL5h5YNBaGNLnwW5UWr42JMeO1k.jpg",
+      "scores":[
+        5,
+        4,
+        1,
+        4,
+        3,
+        1,
+        4,
+        4,
+        5,
+        5
+        ]
+    },
+    {
+      "name":"Jim Parsons",
+      "photo":"https://image.tmdb.org/t/p/w1280/xEL0CTyDiSHoSshuDQFV6qAh9aQ.jpg",
+      "scores":[
+        5,
+        3,
+        1,
+        1,
+        5,
+        5,
+        5,
+        1,
+        1,
+        3
+        ]
+    },
+    {
+      "name":"Johnny Galecki",
+      "photo":"https://image.tmdb.org/t/p/w1280/6SGbRtkZJo8gpC3YtXWe66tb0pO.jpg",
+      "scores":[
+        4,
+        5,
+        2,
+        4,
+        3,
+        2,
+        4,
+        2,
+        4,
+        5
+        ]
+    }];
+    
+    module.exports = friends;
